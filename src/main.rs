8bbfd453b46a0ee5da71f5b0072a5d82513eff2f
Mika Attila <radiantstatue@gmail.com>
2015-10-06 00:01:28 +0200

    Use new API of tagmap

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ fn run() -> i32 {
                     }
                 };
                 let tags = args.collect::<Vec<String>>();
-                for entry in list.tag_map.entries_matching_tags(&tags) {
+                for entry in list.tag_map.matching(&tags) {
                     println!("{}", entry);
                 }
             }
