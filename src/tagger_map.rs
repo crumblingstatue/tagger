dd6ffa3d677ab908a0087b3ecab34cb94c9d0656
Mika Attila <radiantstatue@gmail.com>
2017-05-24 20:43:05 +0200

    Use `?` instead of `try!`

--- src/tagger_map.rs
+++ src/tagger_map.rs
@@ -20,10 +20,10 @@ impl TaggerMap {
         TaggerMap { tag_map: TagMap::new() }
     }
     pub fn from_file<P: AsRef<Path>>(path: P) -> io::Result<Self> {
-        let reader = BufReader::new(try!(fs::File::open(path)));
+        let reader = BufReader::new(fs::File::open(path)?);
         let mut map = TagMap::new();
         for line in reader.lines() {
-            let line = try!(line);
+            let line = line?;
             let quot1 = line.find('"').unwrap();
             let quot2 = line[quot1 + 1..].find('"').unwrap();
             let filename = &line[quot1 + 1..quot2 + 1];
@@ -43,8 +43,8 @@ impl TaggerMap {
         use std::collections::btree_map::Entry;
         let mut added_count = 0;
         // Check for files that aren't part of the list and add them
-        for entry in try!(fs::read_dir(path)) {
-            let entry = try!(entry);
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
             let name = entry.file_name().into_string().unwrap();
             if name != ::LIST_DEFAULT_FILENAME {
                 if let Entry::Vacant(entry) = self.tag_map.entries.entry(name.clone()) {
@@ -69,13 +69,13 @@ impl TaggerMap {
     }
 
     pub fn save_to_file<P: AsRef<Path>>(&self, path: P) -> io::Result<()> {
-        let mut writer = BufWriter::new(try!(fs::File::create(path)));
+        let mut writer = BufWriter::new(fs::File::create(path)?);
         for (k, v) in &self.tag_map.entries {
-            try!(write!(writer, "\"{}\" ", k));
+            write!(writer, "\"{}\" ", k)?;
             for tag in v.iter() {
-                try!(write!(writer, "{} ", tag));
+                write!(writer, "{} ", tag)?;
             }
-            try!(write!(writer, "\n"));
+            write!(writer, "\n")?;
         }
         Ok(())
     }
